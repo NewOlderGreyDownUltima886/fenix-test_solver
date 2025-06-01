15306cb31765e115a1d919cdc6259b25054c8554
krasilniko18 <76857877+krasilniko18@users.noreply.github.com>
2025-06-01 21:39:33 +1000

    Update main2_tg_version.py

--- main2_tg_version.py
+++ main2_tg_version.py
@@ -20,6 +20,18 @@ import prikoli
 import telebot
 from telebot import types, apihelper
 
+print("Добро пожаловать!!! Проверяю обновления...")
+process = subprocess.Popen(['cd ~/fenix-test_solver'], stdout=subprocess.DEVNULL, text=True, shell=True)
+process = subprocess.Popen(['git init'], stdout=subprocess.DEVNULL, text=True, shell=True)
+process = subprocess.Popen(['git stash'], stdout=subprocess.DEVNULL, text=True, shell=True)
+process = subprocess.Popen(['git pull'], stdout=subprocess.PIPE, text=True, shell=True)
+for line in iter(process.stdout.readline, ''):
+    if "Already up to date" in line:
+        print("\nОбновления не найдены, у вас последняя версия!(нестабильная)\n")
+        break
+    elif "Updating" in line:
+        print("\nОбновление установлено успешно, перезапусти меня!!!\n(введи \"test\")\n")
+        quit()
 
 def get_bot_token():
     try:
