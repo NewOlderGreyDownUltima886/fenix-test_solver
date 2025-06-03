530107202fe3f2317195ec32e6ed14f7f050433f
krasilniko18 <76857877+krasilniko18@users.noreply.github.com>
2025-06-03 22:49:18 +1000

    launcher

---
+++ main.py
@@ -0,0 +1,62 @@
+import subprocess, sys
+import time
+i = 1
+
+print("    Выберите версию:\n1. Терминал (старая версия)\n2. Телеграм бот (новая нестабильная версия)")
+chose = 0
+try:
+    while True:
+        print()
+        chose = int(input("\nВыбор (цифра 1 или 2): "))
+        if (chose != 1) or (chose != 2):
+            break
+        else:
+            print("Попробуйте еще раз!\n")
+except:
+    print("Попробуйте еще раз!\n")
+while True:
+    if i > 1:
+        print("Автоматически перезапускаю fenix...")
+    time.sleep(1)
+    print(f"------------------------------------------------------------------------------------------------------------\nЗапускаю fenix-а {i} раз\n")
+    if chose == 1:
+        subprocess.call(["python", "core_test_solver_terminal_edition.py"])
+    elif chose ==2:
+        print("Пока недоступно, извините!")
+        #subprocess.call(["python", "core_test_solver_TgBot_edition.py"])
+    i += 1
+
+
+"""
+def del_msg(chat_id, msg_id_array):  #msg_id - array
+  time.sleep(0.2)
+  try:
+    if len(msg_id_array) != 0:
+      print("\n----------------------------\nПОЛУЧЕН ЗАПРОС НА УДАЛЕНИЕ:", msg_id_array)
+      try:
+        bot.delete_messages(chat_id, msg_id_array)
+        #print("Успешно удалено смс в телеграмме")
+        print("------> УДАЛЕНИЕ ПРОШЛО БЕЗ ОШИБОК, ОЧИЩАЮ МАССИВ")
+        msg_id_array.clear()
+        return True
+      except Exception as e:
+        print('Не получилось удалить смс в телеграмме', msg_id_array, e)
+        print("    ERROR 2: (ошибка удаления смс)", e, " ВХОЖУ ЕЩЕ РАЗ В СЕБЯ ДЛЯ ПОПЫТКИ ЕЩЕ РАЗ!!!!!!!!!!!!!!!")
+        del_msg(chat_id, msg_id_array)
+      
+      try:
+        for i in range(len(msg_id_array)):
+          #print("работаю с", msg_id_array[i])
+          try:
+            bot.delete_message(chat_id, str(msg_id_array[i]))
+            print("Успешно удалено смс в телеграмме", msg_id_array[i] )
+          except Exception as e:
+            print('Не получилось удалить смс в телеграмме', msg_id_array[i], e)
+        print("------> УДАЛЕНИЕ ПРОШЛО БЕЗ ОШИБОК, ОЧИЩАЮ МАССИВ")
+        msg_id_array.clear()
+      
+      except Exception as e:
+        print("    ERROR 2: (ошибка удаления смс)", e, " ВХОЖУ ЕЩЕ РАЗ В СЕБЯ ДЛЯ ПОПЫТКИ ЕЩЕ РАЗ!!!!!!!!!!!!!!!")
+        del_msg(chat_id, msg_id_array)
+      
+"""
