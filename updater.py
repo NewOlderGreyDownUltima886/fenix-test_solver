4d353901081e9b025fd65c66e4f858a39238ffc9
krasilniko18 <76857877+krasilniko18@users.noreply.github.com>
2025-06-04 00:18:37 +1000

    Add files via upload

---
+++ updater.py
@@ -0,0 +1,14 @@
+import os, subprocess
+
+print("Добро пожаловать в Fenix!!! Проверяю обновления...")
+process = subprocess.Popen(['cd ~/fenix-test_solver'], stdout=subprocess.DEVNULL, text=True, shell=True)
+process = subprocess.Popen(['git init'], stdout=subprocess.DEVNULL, text=True, shell=True)
+process = subprocess.Popen(['git stash'], stdout=subprocess.DEVNULL, text=True, shell=True)
+process = subprocess.Popen(['git pull'], stdout=subprocess.PIPE, text=True, shell=True)
+for line in iter(process.stdout.readline, ''):
+    if "Already up to date" in line:
+        print("\nОбновления не найдены, у вас последняя версия!")
+        break
+    elif "Updating" in line:
+        print("\nОбновление установлено успешно!")
+        quit()
